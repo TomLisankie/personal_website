5c6f294df55d8b5bb5e9ad77c969bd7e82b122e1
Emily Xie <emilyxxie@gmail.com>
2016-08-21 18:53:32 -0400

    fixed project hover link problem

---
+++ script.js
@@ -0,0 +1,16 @@
+// $('.project_square').hover(function() {
+//   $(this).find('img').fadeTo(200, 0.2);
+//   $(this).find('p').fadeTo(200, 1);
+// }, function() {
+//   $(this).find('img').fadeTo(200, 1);
+//   $(this).find('p').fadeTo(200, 0);
+// });
+
+
+$('.project_square_background').hover(function() {
+  $(this).find('img').fadeTo(50, 0.5);
+  $(this).find('p').fadeTo(50, 1);
+}, function() {
+  $(this).find('img').fadeTo(50, 1);
+  $(this).find('p').fadeTo(50, 0);
+});
