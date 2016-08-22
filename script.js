366d768bc51125a268df767d04dc669af1b38b99
Emily Xie <emilyxxie@gmail.com>
2016-08-21 20:24:01 -0400

    fixed dropdown problem and about banner overflow

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
 
 
 $('.project_square_background').hover(function() {
-  $(this).find('img').fadeTo(50, 0.5);
-  $(this).find('p').fadeTo(50, 1);
+  $(this).find('img').fadeTo(200, 0.5);
+  $(this).find('p').fadeTo(200, 1);
 }, function() {
-  $(this).find('img').fadeTo(50, 1);
-  $(this).find('p').fadeTo(50, 0);
+  $(this).find('img').fadeTo(200, 1);
+  $(this).find('p').fadeTo(200, 0);
 });
